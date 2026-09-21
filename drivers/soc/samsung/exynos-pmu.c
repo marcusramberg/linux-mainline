SteffenDE/linux-google-tegu

soc: samsung: exynos-pmu: make the rest of the boot-time LPM writes
pmucal_lpm_init[] has 99 entries and mainline made seven of them.  The
omission was recorded as bus clock-gating policy; reading the part of the
table that had only been inferred shows it is not.  It also carries the
SHORTSTOP controls for the CPU clusters, the MIF blocks and NOCL0, the
hardware-change clock muxes for the clusters, the NoCs and CMU_TOP, and four
PWRMGMT_BUNDLE PwrMgmtMode2 bit-31 clears on the MIF blocks -- and
exit_sleep[] clears those same four bits on the way back, so they are live in
the sleep path rather than boot-time tuning.  That matters while "echo mem"
takes the SoC down and nothing brings it back.

Port the subset that targets blocks which are always powered and are part of
the transition itself: the three CPU clusters, the four MIF blocks, the five
NoCs and CMU_TOP, 51 entries.  The ~40 left out are a BUS_COMPONENT_DRCG_EN
sweep across peripheral and camera blocks whose domains this kernel powers
off, and an MMIO access to a gated block stalls the interconnect; they need a
step that can sequence them against their domains, which this is not.

Downstream issues every entry unconditionally, ignoring the condition tuple
the table carries, because at its boot everything is still on.  We honour the
tuple: it names the PMU status bit for the block, so a cluster that is not up
is skipped rather than poked.  That can only skip a write downstream would
have made to a block that was up anyway.

Write semantics are pmucal_rae_write()'s: a full-width mask is a plain write,
anything narrower a read-modify-write of those bits.  None of these are
PMU_ALIVE, so they are ordinary MMIO rather than EL3 calls.  The table is
generated from the vendor header rather than transcribed.

Also fixes a format warning in the CLUSTER0_CPU0_INT_EN diagnostic added
earlier in this branch: BIT(3) is unsigned long.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/c

drivers/soc/samsung/exynos-pmu.c
@@ -12,6 +12,7 @@
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/mfd/core.h>
+#include <linux/io.h>
 #include <linux/moduleparam.h>
 #include <linux/mfd/syscon.h>
 #include <linux/of_platform.h>
@@ -786,7 +787,7 @@ static void zumapro_sys_sleep_arm(void)
 		regmap_read(pmu_context->pmuintrgen,
 			    GS101_GRP1_INTR_BID_UPEND, &upend);
 		pr_info("zumapro: suspend: CLUSTER0_CPU0_INT_EN 0x%x -> wrote 0x%x -> reads 0x%x, GRP1_UPEND 0x%x\n",
-			before, before | BIT(3), after, upend);
+			before, (unsigned int)(before | BIT(3)), after, upend);
 	}
 }
 
@@ -959,6 +960,152 @@ static void zumapro_program_lpm_durations(struct device *dev)
 	}
 }
 
+/*
+ * The rest of downstream's pmucal_lpm_init[], which runs once at boot and which
+ * mainline has never made.  The table has 99 entries; the seven settle
+ * durations above were the only ones ported, on the reading that what remained
+ * was bus clock-gating policy.  It is not: it also carries the SHORTSTOP
+ * controls, the hardware-change clock muxes and the four PWRMGMT_BUNDLE
+ * PwrMgmtMode2 clears on the MIF blocks -- and exit_sleep[] clears those same
+ * four bits on the way back, so they are live in the sleep path rather than
+ * boot-time tuning.
+ *
+ * This is the subset that targets blocks which are always powered and are
+ * involved in the transition itself: the three CPU clusters, the four MIF
+ * blocks, the five NoCs and CMU_TOP.  The ~40 remaining entries are a
+ * BUS_COMPONENT_DRCG_EN sweep across peripheral and camera blocks whose
+ * domains this kernel powers off; writing to a gated block stalls the
+ * interconnect, so they are deliberately left for a later step that can
+ * sequence them against their domains.
+ *
+ * Downstream issues every one of these unconditionally, ignoring the condition
+ * tuple the table carries, because at its boot everything is still on.  We
+ * honour the tuple instead -- it names the PMU status bit for the block, so a
+ * cluster that is not up is skipped rather than poked.  That can only skip a
+ * write downstream would have made to a block that was up anyway.
+ *
+ * Semantics are pmucal_rae_write(): a full-width mask is a plain write, and
+ * anything narrower is a read-modify-write of those bits.  None of these are
+ * PMU_ALIVE, so they are ordinary MMIO rather than EL3 calls.
+ */
+struct zumapro_lpm_init_step {
+	u32 base;
+	u32 offset;
+	u32 mask;
+	u32 val;
+	u32 cond_offset;	/* PMU offset, or 0 for unconditional */
+	u32 cond_mask;
+	u32 cond_val;
+	const char *name;
+};
+
+static const struct zumapro_lpm_init_step zumapro_lpm_init[] = {
+	{ 0x29c00000, 0x085c, 0x0000007f, 0x00000121, 0, 0x0, 0x0, "CPUCL0_HCHGEN_CLKMUX_CPU" },
+	{ 0x29c00000, 0x0854, 0x0000007f, 0x00000101, 0, 0x0, 0x0, "CPUCL0_HCHGEN_CLKMUX_DSU" },
+	{ 0x29c00000, 0x0864, 0x0000007f, 0x00000101, 0, 0x0, 0x0, "CPUCL0_HCHGEN_CLKMUX_BCI" },
+	{ 0x29d00000, 0x0854, 0x0000007f, 0x00000121, 0x1504, 0x1, 0x1, "CPUCL1_HCHGEN_CLKMUX_CPU" },
+	{ 0x29d80000, 0x0854, 0x0000007f, 0x00000121, 0x1704, 0x1, 0x1, "CPUCL2_HCHGEN_CLKMUX_CPU" },
+	{ 0x29c00000, 0x0834, 0xffffffff, 0x8000ffff, 0, 0x0, 0x0, "CPUCL0_CLKDIVSTEP_SMPL_FLT" },
+	{ 0x29c00000, 0x0838, 0xffffffff, 0x00f041c3, 0, 0x0, 0x0, "CPUCL0_CLKDIVSTEP_CON" },
+	{ 0x29c00000, 0x0830, 0xffffffff, 0x00210047, 0, 0x0, 0x0, "CPUCL0_CLKDIVSTEP" },
+	{ 0x29d00000, 0x083c, 0xffffffff, 0x8000ffff, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_SMPL_FLT" },
+	{ 0x29d00000, 0x0834, 0xffffffff, 0xc007f8ff, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_OCP_FLT" },
+	{ 0x29d00000, 0x0838, 0xffffffff, 0xc007f8ff, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_VDROOP_FLT" },
+	{ 0x29d00000, 0x0840, 0xffffffff, 0xfff041c0, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_CON_HEAVY" },
+	{ 0x29d00000, 0x0844, 0xffffffff, 0x00f041c3, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_CON_LIGHT" },
+	{ 0x29d00000, 0x0830, 0xffffffff, 0x00210447, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP" },
+	{ 0x29d80000, 0x083c, 0xffffffff, 0x8000ffff, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_SMPL_FLT" },
+	{ 0x29d80000, 0x0834, 0xffffffff, 0xc007f8ff, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_OCP_FLT" },
+	{ 0x29d80000, 0x0838, 0xffffffff, 0xc007f8ff, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_VDROOP_FLT" },
+	{ 0x29d80000, 0x0840, 0xffffffff, 0xfff041c0, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_CON_HEAVY" },
+	{ 0x29d80000, 0x0844, 0xffffffff, 0x00f041c3, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_CON_LIGHT" },
+	{ 0x29d80000, 0x0830, 0xffffffff, 0x00210447, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP" },
+	{ 0x26040000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "CMU_HCHGEN_CLKMUX" },
+	{ 0x27c00000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x27d00000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x27e00000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x27f00000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x26000000, 0x0840, 0x0000003f, 0x00000001, 0, 0x0, 0x0, "NOCL0_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x26400000, 0x0840, 0x00000001, 0x00000001, 0, 0x0, 0x0, "NOCL1A_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x26800000, 0x0840, 0x0000003f, 0x00000001, 0, 0x0, 0x0, "NOCL1B_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x26c00000, 0x0840, 0x00000001, 0x00000001, 0, 0x0, 0x0, "NOCL2AA_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x27000000, 0x0840, 0x00000001, 0x00000001, 0, 0x0, 0x0, "NOCL2AB_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x29c00000, 0x0824, 0x00000001, 0x00000001, 0, 0x0, 0x0, "CPUCL0_SHORTSTOP_DBG" },
+	{ 0x29c00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "CPUCL0_SHORTSTOP" },
+	{ 0x29d00000, 0x0820, 0x00000001, 0x00000001, 0x1504, 0x1, 0x1, "CPUCL1_SHORTSTOP" },
+	{ 0x29d80000, 0x0820, 0x00000001, 0x00000001, 0x1704, 0x1, 0x1, "CPUCL2_SHORTSTOP" },
+	{ 0x27c00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_SHORTSTOP" },
+	{ 0x27d00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_SHORTSTOP" },
+	{ 0x27e00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_SHORTSTOP" },
+	{ 0x27f00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_SHORTSTOP" },
+	{ 0x26000000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "NOCL0_SHORTSTOP" },
+	{ 0x27c40000, 0xf240, 0x80000000, 0x00000000, 0, 0x0, 0x0, "PWRMGMT_BUNDLE_PwrMgmtMode2" },
+	{ 0x27d40000, 0xf240, 0x80000000, 0x00000000, 0, 0x0, 0x0, "PWRMGMT_BUNDLE_PwrMgmtMode2" },
+	{ 0x27e40000, 0xf240, 0x80000000, 0x00000000, 0, 0x0, 0x0, "PWRMGMT_BUNDLE_PwrMgmtMode2" },
+	{ 0x27f40000, 0xf240, 0x80000000, 0x00000000, 0, 0x0, 0x0, "PWRMGMT_BUNDLE_PwrMgmtMode2" },
+	{ 0x29c20000, 0x0104, 0xffffffff, 0xffffffff, 0, 0x0, 0x0, "BUS_COMPONENT_DRCG_EN" },
+	{ 0x29c20000, 0x010c, 0xffffffff, 0xffffffff, 0, 0x0, 0x0, "BUS_COMPONENT_DRCG_EN_INT" },
+	{ 0x26040000, 0x0880, 0x00000001, 0x00000001, 0, 0x0, 0x0, "EARLY_WAKEUP_DPU_CTRL" },
+	{ 0x26040000, 0x0898, 0xffffffff, 0x000000fe, 0, 0x0, 0x0, "EARLY_WAKEUP_DPU_DEST" },
+	{ 0x26040000, 0x0884, 0x00000001, 0x00000001, 0, 0x0, 0x0, "EARLY_WAKEUP_ISPFE_CTRL" },
+	{ 0x26040000, 0x089c, 0xffffffff, 0x000000fe, 0, 0x0, 0x0, "EARLY_WAKEUP_ISPFE_DEST" },
+	{ 0x26040000, 0x0888, 0x00000001, 0x00000001, 0, 0x0, 0x0, "EARLY_WAKEUP_GSE_CTRL" },
+	{ 0x26040000, 0x08a0, 0xffffffff, 0x000004fc, 0, 0x0, 0x0, "EARLY_WAKEUP_GSE_DEST" },
+};
+
+static void zumapro_program_lpm_init(struct device *dev)
+{
+	unsigned int i, applied = 0, skipped = 0;
+	void __iomem *va = NULL;
+	u32 mapped = 0;
+
+	for (i = 0; i < ARRAY_SIZE(zumapro_lpm_init); i++) {
+		const struct zumapro_lpm_init_step *st = &zumapro_lpm_init[i];
+		u32 reg;
+
+		if (st->cond_offset) {
+			unsigned int cond = 0;
+
+			if (regmap_read(pmu_context->pmureg, st->cond_offset,
+					&cond) ||
+			    (cond & st->cond_mask) != st->cond_val) {
+				skipped++;
+				continue;
+			}
+		}
+
+		if (st->base != mapped) {
+			if (va)
+				iounmap(va);
+			/* PWRMGMT_BUNDLE sits at 0xf240, so a whole 64K window */
+			va = ioremap(st->base, 0x10000);
+			if (!va) {
+				dev_warn(dev, "lpm-init: cannot map %#x\n",
+					 st->base);
+				mapped = 0;
+				skipped++;
+				continue;
+			}
+			mapped = st->base;
+		}
+
+		if (st->mask == ~0u) {
+			writel(st->val, va + st->offset);
+		} else {
+			reg = readl(va + st->offset);
+			reg = (reg & ~st->mask) | (st->val & st->mask);
+			writel(reg, va + st->offset);
+		}
+		applied++;
+	}
+
+	if (va)
+		iounmap(va);
+
+	dev_info(dev, "lpm-init: %u of %u boot-time writes applied, %u skipped\n",
+		 applied, (unsigned int)ARRAY_SIZE(zumapro_lpm_init), skipped);
+}
+
 static int exynos_pmu_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -1062,6 +1209,7 @@ static int exynos_pmu_probe(struct platform_device *pdev)
 			return ret;
 
 		zumapro_program_lpm_durations(dev);
+		zumapro_program_lpm_init(dev);
 		register_syscore(&zumapro_sys_sleep_syscore);
 
 		ret = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,

marcusramberg/linux-mainline

pmucal_lpm_init[] has 99 entries and mainline made seven of them. The
remaining table includes SHORTSTOP controls for the CPU clusters, MIF
blocks and NOCL0, hardware-change clock muxes for the clusters, NoCs and
CMU_TOP, and four PWRMGMT_BUNDLE PwrMgmtMode2 bit-31 clears on the MIF
blocks. exit_sleep[] clears those same four bits on return, so they are
part of the live sleep contract.
Port the 51 entries which target blocks that remain powered through the
transition: the three CPU clusters, four MIF blocks, five NoCs and CMU_TOP.
The peripheral and camera BUS_COMPONENT_DRCG_EN sweep remains excluded
because those domains may already be gated when this driver probes.
Downstream issues every entry unconditionally during early boot. Mainline
honours each table condition and skips an MMIO write if the corresponding
PMU status bit says its block is off.
Write semantics match pmucal_rae_write(): a full-width mask is a plain
write and a narrower mask is a read-modify-write. The table is generated
from the downstream header rather than transcribed. The completion summary
is debug-only.
(cherry picked from commit 7ceebc612807f1cf8ec26074893795c23b3c9a69)
(cherry picked from commit bd25ebab5fead058019be2790566b1db34a3608a)

drivers/soc/samsung/exynos-pmu.c
@@ -919,6 +919,152 @@ static void zumapro_program_lpm_durations(struct device *dev)
 	}
 }
 
+/*
+ * The rest of downstream's pmucal_lpm_init[], which runs once at boot and which
+ * mainline has never made.  The table has 99 entries; the seven settle
+ * durations above were the only ones ported, on the reading that what remained
+ * was bus clock-gating policy.  It is not: it also carries the SHORTSTOP
+ * controls, the hardware-change clock muxes and the four PWRMGMT_BUNDLE
+ * PwrMgmtMode2 clears on the MIF blocks -- and exit_sleep[] clears those same
+ * four bits on the way back, so they are live in the sleep path rather than
+ * boot-time tuning.
+ *
+ * This is the subset that targets blocks which are always powered and are
+ * involved in the transition itself: the three CPU clusters, the four MIF
+ * blocks, the five NoCs and CMU_TOP.  The ~40 remaining entries are a
+ * BUS_COMPONENT_DRCG_EN sweep across peripheral and camera blocks whose
+ * domains this kernel powers off; writing to a gated block stalls the
+ * interconnect, so they are deliberately left for a later step that can
+ * sequence them against their domains.
+ *
+ * Downstream issues every one of these unconditionally, ignoring the condition
+ * tuple the table carries, because at its boot everything is still on.  We
+ * honour the tuple instead -- it names the PMU status bit for the block, so a
+ * cluster that is not up is skipped rather than poked.  That can only skip a
+ * write downstream would have made to a block that was up anyway.
+ *
+ * Semantics are pmucal_rae_write(): a full-width mask is a plain write, and
+ * anything narrower is a read-modify-write of those bits.  None of these are
+ * PMU_ALIVE, so they are ordinary MMIO rather than EL3 calls.
+ */
+struct zumapro_lpm_init_step {
+	u32 base;
+	u32 offset;
+	u32 mask;
+	u32 val;
+	u32 cond_offset;	/* PMU offset, or 0 for unconditional */
+	u32 cond_mask;
+	u32 cond_val;
+	const char *name;
+};
+
+static const struct zumapro_lpm_init_step zumapro_lpm_init[] = {
+	{ 0x29c00000, 0x085c, 0x0000007f, 0x00000121, 0, 0x0, 0x0, "CPUCL0_HCHGEN_CLKMUX_CPU" },
+	{ 0x29c00000, 0x0854, 0x0000007f, 0x00000101, 0, 0x0, 0x0, "CPUCL0_HCHGEN_CLKMUX_DSU" },
+	{ 0x29c00000, 0x0864, 0x0000007f, 0x00000101, 0, 0x0, 0x0, "CPUCL0_HCHGEN_CLKMUX_BCI" },
+	{ 0x29d00000, 0x0854, 0x0000007f, 0x00000121, 0x1504, 0x1, 0x1, "CPUCL1_HCHGEN_CLKMUX_CPU" },
+	{ 0x29d80000, 0x0854, 0x0000007f, 0x00000121, 0x1704, 0x1, 0x1, "CPUCL2_HCHGEN_CLKMUX_CPU" },
+	{ 0x29c00000, 0x0834, 0xffffffff, 0x8000ffff, 0, 0x0, 0x0, "CPUCL0_CLKDIVSTEP_SMPL_FLT" },
+	{ 0x29c00000, 0x0838, 0xffffffff, 0x00f041c3, 0, 0x0, 0x0, "CPUCL0_CLKDIVSTEP_CON" },
+	{ 0x29c00000, 0x0830, 0xffffffff, 0x00210047, 0, 0x0, 0x0, "CPUCL0_CLKDIVSTEP" },
+	{ 0x29d00000, 0x083c, 0xffffffff, 0x8000ffff, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_SMPL_FLT" },
+	{ 0x29d00000, 0x0834, 0xffffffff, 0xc007f8ff, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_OCP_FLT" },
+	{ 0x29d00000, 0x0838, 0xffffffff, 0xc007f8ff, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_VDROOP_FLT" },
+	{ 0x29d00000, 0x0840, 0xffffffff, 0xfff041c0, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_CON_HEAVY" },
+	{ 0x29d00000, 0x0844, 0xffffffff, 0x00f041c3, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP_CON_LIGHT" },
+	{ 0x29d00000, 0x0830, 0xffffffff, 0x00210447, 0x1504, 0x1, 0x1, "CPUCL1_CLKDIVSTEP" },
+	{ 0x29d80000, 0x083c, 0xffffffff, 0x8000ffff, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_SMPL_FLT" },
+	{ 0x29d80000, 0x0834, 0xffffffff, 0xc007f8ff, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_OCP_FLT" },
+	{ 0x29d80000, 0x0838, 0xffffffff, 0xc007f8ff, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_VDROOP_FLT" },
+	{ 0x29d80000, 0x0840, 0xffffffff, 0xfff041c0, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_CON_HEAVY" },
+	{ 0x29d80000, 0x0844, 0xffffffff, 0x00f041c3, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP_CON_LIGHT" },
+	{ 0x29d80000, 0x0830, 0xffffffff, 0x00210447, 0x1704, 0x1, 0x1, "CPUCL2_CLKDIVSTEP" },
+	{ 0x26040000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "CMU_HCHGEN_CLKMUX" },
+	{ 0x27c00000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x27d00000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x27e00000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x27f00000, 0x0850, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x26000000, 0x0840, 0x0000003f, 0x00000001, 0, 0x0, 0x0, "NOCL0_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x26400000, 0x0840, 0x00000001, 0x00000001, 0, 0x0, 0x0, "NOCL1A_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x26800000, 0x0840, 0x0000003f, 0x00000001, 0, 0x0, 0x0, "NOCL1B_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x26c00000, 0x0840, 0x00000001, 0x00000001, 0, 0x0, 0x0, "NOCL2AA_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x27000000, 0x0840, 0x00000001, 0x00000001, 0, 0x0, 0x0, "NOCL2AB_HCHGEN_CLKMUX_CMUREF" },
+	{ 0x29c00000, 0x0824, 0x00000001, 0x00000001, 0, 0x0, 0x0, "CPUCL0_SHORTSTOP_DBG" },
+	{ 0x29c00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "CPUCL0_SHORTSTOP" },
+	{ 0x29d00000, 0x0820, 0x00000001, 0x00000001, 0x1504, 0x1, 0x1, "CPUCL1_SHORTSTOP" },
+	{ 0x29d80000, 0x0820, 0x00000001, 0x00000001, 0x1704, 0x1, 0x1, "CPUCL2_SHORTSTOP" },
+	{ 0x27c00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_SHORTSTOP" },
+	{ 0x27d00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_SHORTSTOP" },
+	{ 0x27e00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_SHORTSTOP" },
+	{ 0x27f00000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "MIF_SHORTSTOP" },
+	{ 0x26000000, 0x0820, 0x00000001, 0x00000001, 0, 0x0, 0x0, "NOCL0_SHORTSTOP" },
+	{ 0x27c40000, 0xf240, 0x80000000, 0x00000000, 0, 0x0, 0x0, "PWRMGMT_BUNDLE_PwrMgmtMode2" },
+	{ 0x27d40000, 0xf240, 0x80000000, 0x00000000, 0, 0x0, 0x0, "PWRMGMT_BUNDLE_PwrMgmtMode2" },
+	{ 0x27e40000, 0xf240, 0x80000000, 0x00000000, 0, 0x0, 0x0, "PWRMGMT_BUNDLE_PwrMgmtMode2" },
+	{ 0x27f40000, 0xf240, 0x80000000, 0x00000000, 0, 0x0, 0x0, "PWRMGMT_BUNDLE_PwrMgmtMode2" },
+	{ 0x29c20000, 0x0104, 0xffffffff, 0xffffffff, 0, 0x0, 0x0, "BUS_COMPONENT_DRCG_EN" },
+	{ 0x29c20000, 0x010c, 0xffffffff, 0xffffffff, 0, 0x0, 0x0, "BUS_COMPONENT_DRCG_EN_INT" },
+	{ 0x26040000, 0x0880, 0x00000001, 0x00000001, 0, 0x0, 0x0, "EARLY_WAKEUP_DPU_CTRL" },
+	{ 0x26040000, 0x0898, 0xffffffff, 0x000000fe, 0, 0x0, 0x0, "EARLY_WAKEUP_DPU_DEST" },
+	{ 0x26040000, 0x0884, 0x00000001, 0x00000001, 0, 0x0, 0x0, "EARLY_WAKEUP_ISPFE_CTRL" },
+	{ 0x26040000, 0x089c, 0xffffffff, 0x000000fe, 0, 0x0, 0x0, "EARLY_WAKEUP_ISPFE_DEST" },
+	{ 0x26040000, 0x0888, 0x00000001, 0x00000001, 0, 0x0, 0x0, "EARLY_WAKEUP_GSE_CTRL" },
+	{ 0x26040000, 0x08a0, 0xffffffff, 0x000004fc, 0, 0x0, 0x0, "EARLY_WAKEUP_GSE_DEST" },
+};
+
+static void zumapro_program_lpm_init(struct device *dev)
+{
+	unsigned int i, applied = 0, skipped = 0;
+	void __iomem *va = NULL;
+	u32 mapped = 0;
+
+	for (i = 0; i < ARRAY_SIZE(zumapro_lpm_init); i++) {
+		const struct zumapro_lpm_init_step *st = &zumapro_lpm_init[i];
+		u32 reg;
+
+		if (st->cond_offset) {
+			unsigned int cond = 0;
+
+			if (regmap_read(pmu_context->pmureg, st->cond_offset,
+					&cond) ||
+			    (cond & st->cond_mask) != st->cond_val) {
+				skipped++;
+				continue;
+			}
+		}
+
+		if (st->base != mapped) {
+			if (va)
+				iounmap(va);
+			/* PWRMGMT_BUNDLE sits at 0xf240, so a whole 64K window */
+			va = ioremap(st->base, 0x10000);
+			if (!va) {
+				dev_warn(dev, "lpm-init: cannot map %#x\n",
+					 st->base);
+				mapped = 0;
+				skipped++;
+				continue;
+			}
+			mapped = st->base;
+		}
+
+		if (st->mask == ~0u) {
+			writel(st->val, va + st->offset);
+		} else {
+			reg = readl(va + st->offset);
+			reg = (reg & ~st->mask) | (st->val & st->mask);
+			writel(reg, va + st->offset);
+		}
+		applied++;
+	}
+
+	if (va)
+		iounmap(va);
+
+	dev_dbg(dev, "lpm-init: %u of %u boot-time writes applied, %u skipped\n",
+		 applied, (unsigned int)ARRAY_SIZE(zumapro_lpm_init), skipped);
+}
+
 static int exynos_pmu_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -1022,6 +1168,7 @@ static int exynos_pmu_probe(struct platform_device *pdev)
 			return ret;
 
 		zumapro_program_lpm_durations(dev);
+		zumapro_program_lpm_init(dev);
 		register_syscore(&zumapro_sys_sleep_syscore);
 
 		ret = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,